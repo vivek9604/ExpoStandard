0af0cb43109453499428fed2716be016f550400b
Vivek Shindhe <vivekshindhe96@gmail>
2022-11-12 19:41:42 +0530

    Init Commit

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import RazorpayCheckout from "react-native-razorpay";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title={"Pay now"} onPress={()=>{
+        const options = {
+          description: 'Credits towards consultation',
+          image: 'https://i.imgur.com/3g7nmJC.png',
+          currency: 'INR',
+          key: 'rzp_test_1DP5mmOlF5G5ag', // Your api key
+          amount: '5000',
+          name: 'foo',
+          prefill: {
+            email: 'void@razorpay.com',
+            contact: '9191919191',
+            name: 'Razorpay Software'
+          },
+          theme: {color: '#F37254'}
+        };
+        RazorpayCheckout.open(options).then((data)=>{
+          alert(JSON.stringify(data))
+        }).catch((error)=>{
+          alert(JSON.stringify(error))
+        })
+      }}/>
     </View>
   );
 }
